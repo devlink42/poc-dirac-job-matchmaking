f09ab1c9ffe2d2fa11324230cbf40a942f758108
ychabani <yan.chabanis@cern.ch>
2026-04-01 18:02:47 +0200

    feat: add Job model and update dependencies

---
+++ src/models/job.py
@@ -0,0 +1,38 @@
+from typing import Union
+
+from pydantic import BaseModel
+
+
+class System(BaseModel):
+    name: str
+    glibc: str
+    user_namespaces: str
+
+
+class Request(BaseModel):
+    overhead: int
+    per_core: int
+
+
+class Limit(BaseModel):
+    overhead: int
+    per_core: int
+
+
+class Architecture(BaseModel):
+    name: str
+    microarchitecture_level: dict[str, int]
+
+
+class Cpu(BaseModel):
+    num_cores: dict[str, int]
+    ram_mb: Union[Request, Limit]
+    architecture: Architecture
+
+
+class Job(BaseModel):
+    jobID: str
+    system: System
+    wall_time: int
+    cpu_work: int
+    cpu: Cpu
